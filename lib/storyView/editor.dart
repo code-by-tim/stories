76df89f15ebc90f293e944bca132b9aa5b16c8b9
TimBaechle <st167752@stud.uni-stuttgart.de>
2021-04-08 11:12:52 +0200

    Added Comment

--- lib/storyView/editor.dart
+++ lib/storyView/editor.dart
@@ -7,5 +7,7 @@ class Editor extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container();
     //Build the ListView of SmartTextFields here
+    //and show the toolbar if the keyboard is visible.
+    //I don't need to implement keyboard actions, that is all handled by the TextFields
   }
 }
