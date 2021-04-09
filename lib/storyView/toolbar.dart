117533a8c55b10234dbcc86f820faa22f4d81585
TimBaechle <st167752@stud.uni-stuttgart.de>
2021-04-09 02:31:19 +0200

    Implemented Toolbar :)

--- lib/storyView/toolbar.dart
+++ lib/storyView/toolbar.dart
@@ -10,29 +10,60 @@ class Toolbar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        IconButton(
-          icon: Icon(Icons.bathtub),
-          onPressed: () => onSelected,
+    return PreferredSize(
+      // Matches the expected size of the list in the stack of the editor
+      preferredSize: Size.fromHeight(56.0),
+      child: Material(
+        // elevation: 4.0,
+        color: Colors.grey,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            IconButton(
+              icon: Icon(Icons.format_size),
+              color: selectedType == SmartContentType.HEADING
+                  ? Colors.teal
+                  : Colors.black,
+              onPressed: () => onSelected,
+            ),
+            IconButton(
+              icon: Icon(Icons.format_quote),
+              color: selectedType == SmartContentType.QUOTE
+                  ? Colors.teal
+                  : Colors.black,
+              onPressed: () => onSelected,
+            ),
+            IconButton(
+              icon: Icon(Icons.format_list_bulleted),
+              color: selectedType == SmartContentType.BULLET
+                  ? Colors.teal
+                  : Colors.black,
+              onPressed: () => onSelected,
+            ),
+            IconButton(
+              icon: Icon(Icons.format_list_numbered),
+              color: selectedType == SmartContentType.NUMERATION
+                  ? Colors.teal
+                  : Colors.black,
+              onPressed: () => onSelected,
+            ),
+            IconButton(
+              icon: Icon(Icons.mic),
+              color: selectedType == SmartContentType.NUMERATION
+                  ? Colors.teal
+                  : Colors.black,
+              onPressed: () => onSelected,
+            ),
+            IconButton(
+              icon: Icon(Icons.photo_camera),
+              color: selectedType == SmartContentType.NUMERATION
+                  ? Colors.teal
+                  : Colors.black,
+              onPressed: () => onSelected,
+            ),
+          ],
         ),
-        IconButton(
-          icon: Icon(Icons.ac_unit_outlined),
-          onPressed: () => onSelected,
-        ),
-        IconButton(
-          icon: Icon(Icons.ac_unit_outlined),
-          onPressed: () => onSelected,
-        ),
-        IconButton(
-          icon: Icon(Icons.ac_unit_outlined),
-          onPressed: () => onSelected,
-        ),
-        IconButton(
-          icon: Icon(Icons.ac_unit_outlined),
-          onPressed: () => onSelected,
-        ),
-      ],
+      ),
     );
   }
 }
